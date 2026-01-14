014f49c5b3e45ba68800737d07e20562078407bd
gzifoto-spec <gzifoto@gmail.com>
2026-01-14 16:37:15 +0100

    ci: style added for footer at index.css

--- js/footer.js
+++ js/footer.js
@@ -4,8 +4,8 @@ class MyFooter extends HTMLElement {
         <footer class="footer" id="footerMenu">
             <div class="footer-content">
                 <p class="footer-text">
-                    code licensed under <a href="https://www.gnu.org/licenses/old-licenses/gpl-2.0.html#SEC1"><img src="../assets/img/icons/gnuTranspSmall.png" alt="GNU icon">GPL-2 license</a>
-                    API © 2022-2026 Copyright: Open-Meteo.com
+                    code: <a href="https://www.gnu.org/licenses/old-licenses/gpl-2.0.html#SEC1"><img class="footer-icon" src="../assets/img/icons/gnuTranspSmall.png" alt="GNU icon">GPL-2 license</a><br>
+                    © 2022-2026 Copyright: Open-Meteo.com
                 </p>
             </div>
         </footer>
